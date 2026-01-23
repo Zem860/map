3037973ae0bb3f933ec72906cbf38ce7d42ab221
Zem860 <zemmyhammy@gmail.com>
2026-01-23 23:29:41 +0800

    upate:add type carts

---
+++ src/type/cart.ts
@@ -0,0 +1,44 @@
+import type { productData } from "./product"
+// import type { couponData } from "./coupon"
+export type cartData = {
+    data: {
+        product_id: string,
+        qty: number,
+        total: number,
+        final_total: number,
+        product: productData,
+        id: string
+    }
+}
+
+export type getCartData = {
+    data: {
+        product_id: string,
+        qty: number,
+        total: number,
+        final_total: number,
+        product: productData,
+        id: string
+    }
+}
+
+
+/** Cart 裡面的單筆 item */
+export type CartItem = {
+    id: string
+    product_id: string
+    qty: number
+    total: number
+    final_total: number
+    product: productData
+    // coupon?: couponData
+}
+
+/** /cart 回傳的 data 區塊 */
+export type shoppingCartData = {
+    data: {
+        carts: CartItem[]
+        total: number
+        final_total: number
+    }
+}
