ae1ae8d7808fd3bd6667ca75a15bf2875d7a66e4
Zem860 <zemmyhammy@gmail.com>
2026-01-25 12:30:59 +0800

    update:增加margin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return ( 
-    <footer className="border-t py-12 bg-background">
+    <footer className="border-t py-12 bg-background mt-5">
         <div className="container mx-auto">
           <div className="grid grid-cols-2 md:grid-cols-4 gap-8 mb-8">
             <div>
