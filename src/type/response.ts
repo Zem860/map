68d5d01a1c55e2c13c2ebab62540c5c2c0eb9604
Zem860 <zemmyhammy@gmail.com>
2026-01-25 11:28:49 +0800

    update:移除多於空行

--- src/type/response.ts
+++ src/type/response.ts
@@ -1,10 +1,8 @@
 import type { productData, PaginationData } from "./product";
-import type { cartData, shoppingCartData } from "./cart";
+import type { CartData, cartData, UpdateQtyParams } from "./cart";
 export type MesssageResponse = {
-
     success: boolean,
     message: string,
-
 }
 
 export type GetProductsResponse = {
@@ -14,23 +12,19 @@ export type GetProductsResponse = {
     messages: string[]
 }
 
-
 export type GetProductResponse = {
     success: boolean,
     product: productData,
 }
 
-export type PostCartResponse = MesssageResponse & cartData
-export type getCartResponse = MesssageResponse & shoppingCartData
-
+export type PostCartResponse = MesssageResponse & cartData;
+export type UpdateCartResponse = MesssageResponse & UpdateQtyParams;
+export type getCartResponse = MesssageResponse & CartData[];
 
 export type UploadImageResponse = {
     success: boolean,
     imageUrl: string,
 }
-
-
-
 
 export type OrderResponse = {
     total:string,
