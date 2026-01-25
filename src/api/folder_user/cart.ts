c795dc5a888a6bfa10886959642354a62ba14514
Zem860 <zemmyhammy@gmail.com>
2026-01-25 11:26:44 +0800

    update:移除console.log的bug

--- src/api/folder_user/cart.ts
+++ src/api/folder_user/cart.ts
@@ -1,13 +1,17 @@
-import type { MesssageResponse, PostCartResponse, getCartResponse } from "@/type/response"
+import type { MesssageResponse, PostCartResponse, UpdateCartResponse, getCartResponse } from "@/type/response"
 import type { AxiosResponse } from "axios";
 import { baseApi } from "../api";
+import type { UpdateQtyParams } from "@/type/cart";
 
 type addToCartFunc = (params:{product_id:string, qty:number}) =>Promise<AxiosResponse<PostCartResponse>>
 
-export const postCart:addToCartFunc = (params:{product_id:string, qty:number})=>baseApi.post('/cart', {data:params})
+export const postCart:addToCartFunc = (params:UpdateQtyParams)=>baseApi.post('/cart', {data:params})
 
 type getCartFunc= ()=>Promise<AxiosResponse<getCartResponse>>
 export const getCart:getCartFunc = ()=>baseApi.get('/cart')
 
 type deleteProductFromCartFunc = (product_id:string)=>Promise<AxiosResponse<MesssageResponse>>
 export const deleteProductFromCartFunc:deleteProductFromCartFunc =(product_id:string)=>baseApi.delete(`/cart/${product_id}`)
+
+type updateCartQtyFunc = (params:UpdateQtyParams)=>Promise<AxiosResponse<UpdateCartResponse>>
+export const updateCartQtyFunc = (params:UpdateQtyParams)=>baseApi.put(`/cart/${params.product_id}`, {data:params})
