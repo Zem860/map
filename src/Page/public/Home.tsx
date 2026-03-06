72527ef4f7ffab78d1249c80ddb8e9149137a225
Zem860 <zemmyhammy@gmail.com>
2026-03-06 11:48:57 +0800

    udpate:將首頁的new arrivals排版改為在sm時不並排顯示兩個，改成顯示一個卡片

--- src/Page/public/Home.tsx
+++ src/Page/public/Home.tsx
@@ -165,7 +165,7 @@ const Home = () => {
               <ArrowRight className="ml-2 h-4 w-4" />
             </Button>
           </div>
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
             {
 
               newArrivals.map((item: productData) => {
