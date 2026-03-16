ce0e89f79ee7572ecc233fbe943147c0f788e0c2
Zem860 <zemmyhammy@gmail.com>
2026-03-16 10:26:27 +0800

    refactor:將回首業的按鈕樣式直接套道連結上

--- src/Page/public/Payment.tsx
+++ src/Page/public/Payment.tsx
@@ -228,9 +228,7 @@ const Payment = () => {
                                     <ChevronRight className="w-5 h-5" />
                                 </button>
                                 :
-                                <div className="w-full md:w-auto md:px-12 py-4 bg-primary text-primary-foreground font-bold rounded-lg shadow-lg hover:brightness-95 active:scale-[0.98] transition-all flex items-center justify-center gap-2 text-lg">
-                                    <Link to="/">Back to Homepage</Link>
-                                </div>
+                                <Link className="w-full md:w-auto md:px-12 py-4 bg-primary text-primary-foreground font-bold rounded-lg shadow-lg hover:brightness-95 active:scale-[0.98] transition-all flex items-center justify-center gap-2 text-lg" to="/">Back to Homepage</Link>
                             }
                         </div>
                     </div>
